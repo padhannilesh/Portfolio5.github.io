98de8d669a3a6d89f3304833c78837f49d005420
Nilesh Kumar Padhan <112704460+padhannilesh@users.noreply.github.com>
2024-07-27 00:59:43 +0530

    Update Sample.js

--- Sample.js
+++ Sample.js
@@ -1,1 +1,19 @@
-
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+    <script
+      src="https://kit.fontawesome.com/58a810656e.js"
+      crossorigin="anonymous"
+    ></script>
+    <script>
+      var typeData = new Typed(".role", {
+        strings: [
+          "Full Stack Developer",
+          "Frontend Developer",
+          "Competitive Programmer",
+          "Coder",
+        ],
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 80,
+        backDelay: 1000,
+      });
+    </script>
